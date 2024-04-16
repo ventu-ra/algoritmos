cb3dc9b2453028ee1af93af3ec4083832b9c8949
ventu-ra <ventura09.10.0@gmail.com>
2024-04-16 20:26:45 -0300

    implementação do algoritmo de pesquisa binaria em rust

--- pesquisa_binaria_em_rust/src/main.rs
+++ pesquisa_binaria_em_rust/src/main.rs
@@ -1,3 +1,26 @@
+fn pesquisa_binaria(lista: &[i32], item: i32) -> Option<usize> {
+    let mut baixo = 0;
+    let mut alto = lista.len() - 1;
+
+    while baixo <= alto {
+        let meio = (baixo + alto) / 2;
+        let chute = lista[meio];
+
+        if chute == item {
+            return Some(meio);
+        }
+        if chute > item {
+            alto = meio - 1;
+        } else {
+            baixo = meio + 1;
+        }
+    }
+    return None;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lista = [1, 3, 5, 7, 9];
+
+    println!("{:?}", pesquisa_binaria(&lista, 5)); // Saída: Some(1)
+                                                   // println!("{:?}", pesquisa_binaria(&lista, -1)); // Saída: None
 }
