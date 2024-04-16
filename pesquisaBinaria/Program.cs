a20a5991565ba8a131839e88e7891ae1078c8da5
ventu-ra <ventura09.10.0@gmail.com>
2024-04-16 19:43:26 -0300

    implementação do algoritmo de pesquisa binaria

--- pesquisaBinaria/Program.cs
+++ pesquisaBinaria/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static int pesquisaBinaria(int[] lista, int item)
+{
+  int baixo = 0;
+
+  int alto = lista.Length - 1;
+
+  while (baixo <= alto)
+  {
+    int meio = (baixo + alto) / 2;
+    int chute = lista[meio];
+
+    if (chute == item) return meio;
+
+    if (chute > item) alto = meio - 1;
+
+    else baixo = meio + 1;
+
+  }
+
+  return -1;
+}
+
+int[] lista = { 1, 3, 5, 7, 9 };
+
+Console.WriteLine(pesquisaBinaria(lista, 3));
+Console.WriteLine(pesquisaBinaria(lista, -1));
